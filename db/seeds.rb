dad2aebe90c8e7821de8f20a42ffedce1030dc11
Sergio Gomes <sergiofgomes117@gmail.com>
2018-04-05 17:09:48 -0600

    created blogs model and added seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+blogs = Blog.create(
+  [
+    {
+      title: "How to create a rails api",
+      body: "it's not too hard!"
+    },
+     {
+      title: "Coffee basics",
+      body: "cold brew = da best asdfhasdfhaskdjfhaskjdfhasdkjfhaksdljfhaskdjfhaksjdfhkasjdfhksajdhfks"
+    },
+    {
+      title: "One more generic blog",
+      body: "the cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a liethe cake is a lie"
+    }
+  ]
+)
